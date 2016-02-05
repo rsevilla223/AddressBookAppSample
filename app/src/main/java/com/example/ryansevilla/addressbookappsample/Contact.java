874c95b23fd6a37960bb213dbe2a0064e0455447
ryansevilla <rsevilla@luc.edu>
2016-02-04 20:47:36 -0600

    added contact class

---
+++ app/src/main/java/com/example/ryansevilla/addressbookappsample/Contact.java
@@ -0,0 +1,57 @@
+package com.example.ryansevilla.addressbookappsample;
+
+/**
+ * Created by ryansevilla on 2/4/16.
+ */
+public class Contact {
+    private String name;
+    private String company;
+    private String home_number;
+    private String mobile_number;
+    private String work_number;
+    private int birthdate;
+    private int employeeId;
+
+
+    public Contact(String name, String company,
+                   int birthdate, int EmployeeId,
+                   String home_number, String mobile_number,
+                   String work_number ) {
+        super();
+        this.name = name;
+        this.company = company;
+        this.birthdate = birthdate;
+        this.employeeId = employeeId;
+        this.home_number = home_number;
+        this.mobile_number = mobile_number;
+        this.work_number = work_number;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getCompany(){
+        return company;
+    }
+
+    public String getHome_number(){
+        return home_number;
+    }
+
+    public String getMobile_number(){
+        return mobile_number;
+    }
+
+    public String getWork_number(){
+        return work_number;
+    }
+
+    public int getBirthdate(){
+        return birthdate;
+    }
+
+    public int getEmployeeId(){
+        return employeeId;
+    }
+}
